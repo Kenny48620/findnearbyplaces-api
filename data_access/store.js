dcf86f7404ffb580405f9be4b4261ba6b9a8ec20
Kenny48620 <kennyleetrade@gmail.com>
2022-05-11 19:35:12 -0700

    check

---
+++ data_access/store.js
@@ -0,0 +1,197 @@
+const bcrypt = require('bcrypt');
+const { Pool } = require('pg');
+require('dotenv').config();
+
+const connectionString = `postgres://${process.env.USERNAME}:${process.env.PASSWORD}@${process.env.HOST}:${process.env.DATABASEPORT}/${process.env.DATABASE}`;
+
+const connection = {
+    connectionString: process.env.DATABASE_URL ? process.env.DATABASE_URL : connectionString,
+    ssl: { rejectUnauthorized: false }
+}
+
+const pool = new Pool(connection);
+
+let store = {
+    getSearch: (search_term, user_location, radius_filter, maximum_results_to_return, category_filter, sort) => {
+        //user_location will be a string with two comma separated values, latitude and longitude
+        //required: search_term, user_location, maximum_results_to_return
+        //optional: radius_filter, category_filter, sort
+        let query = `select l.name as name, CONCAT(l.latitude,',', l.longitude) as location, c.name as category, r.rating as rating, p.file as thumbnail 
+                    from findnearbyplaces.locations l 
+                    join findnearbyplaces.category c on c.id = l.category_id   
+                    join findnearbyplaces.reviews r on r.location_id = l.id
+                    join findnearbyplaces.place_photo pp on pp.location_id = l.id
+                    join findnearbyplaces.photo p on p.id = pp.photo_id
+                    where l.name like CONCAT('%',$1,'%')  
+                    `;
+        if (category_filter) {
+            query += ` or c.name like CONCAT('%',$5,'%')`;
+        } else {
+            query += ` or c.name like CONCAT('%',$1,'%')`;
+        }
+
+        if (radius_filter) {
+            query += ` and CONCAT(l.latitude,',', l.longitude) <= $2 + $3 and location >= $2 - $3`;
+        } else {
+            query += ` and CONCAT(l.latitude,',', l.longitude) like $2`;
+        }
+
+        if (maximum_results_to_return) {
+            query += ` limit $4`;
+        }
+
+        return pool.query(query,[search_term, user_location, radius_filter, maximum_results_to_return, category_filter, sort])
+        .then(x => {
+            console.log('from store: ', x);
+            if (x.rows.length >0) {
+                return {done: true, result: x.rows};
+            } else {
+                return {done: false, result: x.rows};
+            }
+        })
+        .catch(e => {
+            console.log(e);
+        })
+    },
+
+    addCustomer: (email, password) => {
+        const hash = bcrypt.hashSync(password, 10);
+        return pool.query('INSERT INTO findnearbyplaces.customer (email, password) VALUES ($1, $2)', [email, hash]);
+    },
+
+    login: (email, password) => {
+        return pool.query('SELECT id, email, password FROM findnearbyplaces.customer WHERE email = $1', [email])
+            .then(x => {
+                if (x.rows.length == 1) {
+                    let valid = bcrypt.compareSync(password, x.rows[0].password);
+                    if (valid) {
+                        return { valid: true, user: {id: x.rows[0].id, username: x.rows[0].email}};
+                    } else {
+                        return { valid: false, message: 'Credentials are not valid.' };
+                    }
+                } else {
+                    return { valid: false, message: 'Email not found.' };
+                }
+            });
+    },
+
+    postLocation: (name,category_id,latitude,longitude,description) => {
+        return pool.query('INSERT INTO findnearbyplaces.locations (name,category_id,latitude,longitude,description) VALUES ($1, $2, $3, $4, $5)', [name,category_id,latitude,longitude,description])
+    },
+
+    getLocationId: (name) => {
+        return pool.query('SELECT l.id FROM findnearbyplaces.locations l WHERE name LIKE $1', [name])
+    },
+
+    postCategory: (name) => {
+        return pool.query('INSERT INTO findnearbyplaces.category (name) VALUES ($1)', [name])
+    },
+
+    getCategoryId: (name) => {
+        return pool.query('SELECT c.id FROM findnearbyplaces.category c WHERE name LIKE $1', [name])
+    },
+
+    postPhoto: (photo) => {
+        return pool.query('INSERT INTO findnearbyplaces.photo (file) VALUES ($1)', [photo])
+    },
+
+    getPhotoId: (photo) => {
+        return pool.query('SELECT p.id FROM findnearbyplaces.photo p WHERE file = $1', [photo])
+    },
+
+    postPlacePhoto: (place_id, photo_id) => {
+        return pool.query('INSERT INTO findnearbyplaces.place_photo (location_id, photo_id) VALUES ($1, $2)', [place_id, photo_id])
+    },
+
+    postReviewPhoto: (review_id, photo_id) => {
+        return pool.query('INSERT INTO findnearbyplaces.review_photo (review_id, photo_id) VALUES ($1, $2)', [review_id, photo_id])
+    },
+
+    postReview: (location_id, text, rating) => {
+        return pool.query('INSERT INTO findnearbyplaces.reviews (location_id, text, rating) VALUES ($1, $2, $3)', [location_id, text, rating])
+    },
+
+    getReviewId: (location_id, text, rating) => {
+        return pool.query('SELECT r.id FROM findnearbyplaces.reviews r WHERE location_id = $1 AND text = $2 AND rating = $3', [location_id, text, rating])
+    },
+
+    putPlace: (place_id, name, category_id, latitude, longitude, description) => {
+        let result = [place_id];
+        let query = 'UPDATE findnearbyplaces.locations SET';
+        if (name) {
+            if (result.length > 1) {
+                query += ',';
+            }
+            query += ' name = $2';
+            result.push(name);
+        }
+        if (category_id) {
+            if (result.length > 1) {
+                query += ',';
+            }
+            query += ' category_id = $3';
+            result.push(category_id);
+        }
+        if (latitude) {
+            if (result.length > 1) {
+                query += ',';
+            }
+            query += ' latitude = $4';
+            result.push(latitude);
+        }
+        if (longitude) {
+            if (result.length > 1) {
+                query += ',';
+            }
+            query += ' longitude = $5';
+            result.push(longitude);
+        }
+        if (description) {
+            if (result.length > 1) {
+                query += ',';
+            }
+            query += ' description = $6';
+            result.push(description);
+        }
+        query += ' WHERE id = $1';
+        return pool.query(query, result)
+    },
+
+    putReview: (review_id, text, rating) => {
+        let result = [review_id];
+        let query = 'UPDATE findnearbyplaces.reviews SET';
+        if (text) {
+            query += ' text = $2';
+            result.push(text);
+        }
+        if (rating) {
+            if (result.length > 1) {
+                query += ',';
+            }
+            query += ' rating = $3';
+            result.push(rating);
+        }
+        query += ' WHERE id = $1';
+        console.log(query);
+        return pool.query(query, result)
+    },
+
+    putPhoto: (photo_id, file) => {
+        return pool.query('UPDATE findnearbyplaces.photo SET file = $2 WHERE id = $1', [photo_id, file])
+    },
+
+    deletePlace: (place_id) => {
+        return pool.query('DELETE FROM findnearbyplaces.locations WHERE id = $1', [place_id])
+    },
+
+    deleteReview: (review_id) => {
+        return pool.query('DELETE FROM findnearbyplaces.reviews WHERE id = $1', [review_id])
+    },
+
+    deletePhoto: (photo_id) => {
+        return pool.query('DELETE FROM findnearbyplaces.photo WHERE id = $1', [photo_id])
+    }
+}
+
+
+module.exports = {store};
---
+++ index.js
@@ -0,0 +1,299 @@
+const express = require('express');
+var cors = require('cors');
+var passport = require('passport');
+var LocalStrategy = require('passport-local');
+var session = require('express-session');
+var SQLiteStore = require('connect-sqlite3')(session);
+const {store} = require('./data_access/store');
+const req = require('express/lib/request');
+const { Pool } = require('pg/lib');
+
+const app = express();
+
+const port = process.env.PORT || 4002;
+
+
+//middlewares
+app.use(cors({
+    origin: "https://kenny48620.github.io",
+    credentials: true
+}));
+app.use(express.json());
+
+
+
+
+app.use((request, response, next) => {
+    console.log(`request url: ${request.url}`);
+    console.log(`request method: ${request.method}`);
+    //only for development. Remove the next two lines when you deploy your final version.
+    console.log(`request body:`);
+    console.log(request.body);
+    next();
+})
+
+passport.use(
+    new LocalStrategy({ usernameField: 'email' }, function verify(username, password, cb) {
+        store.login(username, password)
+            .then(x => {
+                if (x.valid) {
+                    return cb(null, x.user);
+                } else {
+                    return cb(null, false, { message: 'Incorrect username or password.' });
+                }
+            })
+            .catch(e => {
+                console.log(e);
+                cb('Something went wrong!');
+            });
+
+    }));
+
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false,
+    store: new SQLiteStore({ db: 'sessions.db', dir: './sessions' })
+}));
+app.use(passport.authenticate('session'));
+
+passport.serializeUser(function (user, cb) {
+    process.nextTick(function () {
+        cb(null, { id: user.id, username: user.username });
+    });
+});
+
+passport.deserializeUser(function (user, cb) {
+    process.nextTick(function () {
+        return cb(null, user);
+    });
+});
+
+app.get('/', (req,res) => {
+    res.status(200).json({done: true, message: 'This is the backend for findnearbyplaces '});
+});
+ 
+//1
+app.get('/search/:search_term/:user_location/:radius_filter/:maximum_results_to_return/:category_filter/:sort', (req, res) => {
+    let search_term = req.params.search_term;
+    let user_location = req.params.user_location;
+    let radius_filter = req.params.radius_filter;
+    let maximum_results_to_return = req.params.maximum_results_to_return;
+    let category_filter = req.params.category_filter;
+    let sort = req.params.sort;
+    store.getSearch(search_term, user_location, radius_filter, maximum_results_to_return, category_filter, sort)
+    .then(x => {
+        console.log(x);
+        if (x) {
+            res.status(200).json(x);
+        } else {
+            res.status(404).json(x);
+        }
+    })
+})
+
+//2
+app.post('/register', (req, res) => {
+    let email = req.body.email;
+    let password = req.body.password; /* store will handle encryption */
+    store.addCustomer(email, password)
+        .then(x => {
+            //console.log(x);
+            res.status(200).json({ done: true, message: 'A customer has been added successfully' })
+        })
+        .catch(e => {
+            console.log(e);
+            res.status(500).json({ done: false, message: 'Customer not added due to an error.' });
+        });
+
+});
+
+app.post('/login', passport.authenticate('local', {
+    successRedirect: '/login/succeeded',
+    failureRedirect: '/login/failed'
+}));
+app.get('/login/succeeded', (request, response) => {
+    response.status(200).json({ done: true, message: 'The customer logged in successfully.' });
+});
+
+app.get('/login/failed', (request, response) => {
+    response.status(401).json({ done: false, message: 'The credentials are not valid.' });
+});
+
+//3
+app.post('/place', (req, res) => {
+    let name = req.body.name;
+    let category_id = req.body.category_id;
+    let latitude = req.body.latitude;
+    let longitude = req.body.longitude;
+    let description = req.body.description;
+    store.postLocation(name,category_id,latitude,longitude,description)
+    .then(x => {
+        return store.getLocationId(name)
+    })
+    .then(x => {
+        res.status(200).json({done: true, id: x.rows[0].id, message: 'Location added successfully'});
+    })
+    .catch(e => {
+        console.log(e);
+        res.status(500).json({ done: false, message: 'Location not added due to an error.' });
+    })
+});
+
+//3.1
+app.post('/category', (req, res) => {
+    let name = req.body.name;
+    store.postCategory(name)
+    .then(x => {
+        return store.getCategoryId(name)
+    })
+    .then(x => {
+        res.status(200).json({done: true, id: x.rows[0].id, message: 'Category added successfully'});
+    })
+    .catch(e => {
+        console.log(e);
+        res.status(500).json({ done: false, message: 'Category not added due to an error.' });
+    })
+});
+
+//4
+app.post('/photo', (req, res) => {
+    let photo = req.body.photo;
+    let place_id = req.body.place_id;
+    let review_id = req.body.review_id;
+    var photo_id;
+    store.postPhoto(photo)
+    .then(x => {
+        return store.getPhotoId(photo)
+    })
+    .then(x => {
+        photo_id = x.rows[0].id;
+        if (place_id) {
+            return store.postPlacePhoto(place_id, photo_id)
+        } else if (review_id) {
+            return store.postReviewPhoto(review_id, photo_id)
+        }
+    })
+    .then(x => {
+        res.status(200).json({done: true, id: photo_id, message: 'Photo added successfully'});
+    })
+    .catch(e => {
+        console.log(e);
+        res.status(500).json({ done: false, message: 'Photo not added due to an error.' });
+    })
+});
+
+//5
+app.post('/review', (req, res) => {
+    let location_id = req.body.place_id;
+    let text = req.body.comment;
+    let rating = req.body.rating;
+    store.postReview(location_id, text, rating)
+    .then(x => {
+        return store.getReviewId(location_id, text, rating)
+    })
+    .then(x => {
+        res.status(200).json({done: true, id: x.rows[0].id, message: 'Review added successfully'});
+    })
+    .catch(e => {
+        console.log(e);
+        res.status(500).json({ done: false, message: 'Review not added due to an error.' });
+    })
+});
+
+//6
+app.put('/place', (req, res) => {
+    let place_id = req.body.place_id;
+    let name = req.body.name;
+    let category_id = req.body.category_id;
+    let latitude = req.body.latitude;
+    let longitude = req.body.longitude;
+    let description = req.body.description;
+    store.putPlace(place_id, name, category_id, latitude, longitude, description)
+    .then(x => {
+        res.status(200).json({done: true, message: 'Location updated successfully'});
+    })
+    .catch(e => {
+        console.log(e);
+        res.status(500).json({ done: false, message: 'Location not updated due to an error.' });
+    })
+});
+
+//7
+app.put('/review', (req, res) => {
+    let review_id = req.body.review_id;
+    let text = req.body.comment;
+    let rating = req.body.rating;
+    store.putReview(review_id, text, rating)
+    .then(x => {
+        res.status(200).json({done: true, message: 'Review updated successfully'});
+    })
+    .catch(e => {
+        console.log(e);
+        res.status(500).json({ done: false, message: 'Review not updated due to an error.' });
+    })
+});
+
+//8
+app.put('/photo', (req, res) => {
+    let photo_id = req.body.photo_id;
+    let file = req.body.photo;
+    store.putPhoto(photo_id, file)
+    .then(x => {
+        res.status(200).json({done: true, message: 'Photo updated successfully'});
+    })
+    .catch(e => {
+        console.log(e);
+        res.status(500).json({ done: false, message: 'Photo not updated due to an error.' });
+    })
+});
+
+//9
+app.delete('/place', (req, res) => {
+    let place_id = req.body.place_id;
+    store.deletePlace(place_id)
+    .then(x => {
+        res.status(200).json({done: true, message: 'Place deleted successfully'});
+    })
+    .catch(e => {
+        console.log(e);
+        res.status(500).json({ done: false, message: 'Place not deleted due to an error.' });
+    })
+});
+
+//10
+app.delete('/review', (req, res) => {
+    let review_id = req.body.review_id;
+    store.deleteReview(review_id)
+    .then(x => {
+        res.status(200).json({done: true, message: 'Review deleted successfully'});
+    })
+    .catch(e => {
+        console.log(e);
+        res.status(500).json({ done: false, message: 'Review not deleted due to an error.' });
+    })
+});
+
+//11
+app.delete('/photo', (req, res) => {
+    let photo_id = req.body.photo_id;
+    store.deletePhoto(photo_id)
+    .then(x => {
+        res.status(200).json({done: true, message: 'Photo deleted successfully'});
+    })
+    .catch(e => {
+        console.log(e);
+        res.status(500).json({ done: false, message: 'Photo not deleted due to an error.' });
+    })
+});
+
+//listen
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
+
+//logout
+app.post('/logout', (req,res) => {
+    req.logout();
+    res.redirect('/');
+});
